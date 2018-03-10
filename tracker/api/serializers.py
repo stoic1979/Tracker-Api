421772262af9415536fde4118e59d4c8bab21489
Navjot Singh <weavebytes@gmail.com>
2018-03-11 02:46:20 +0530

    tweaks

--- tracker/api/serializers.py
+++ tracker/api/serializers.py
@@ -6,7 +6,7 @@ from tracker.api.models import Child, ChildLocation, ChildDevice
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):

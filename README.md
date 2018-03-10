47a41be56f84e64f25ee7e625831202de793a7a8
Navjot Singh <weavebytes@gmail.com>
2018-03-10 23:54:31 +0530

    refactored code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tracker-Api
 Tracker APIs in django, postgresql and django rest framework
+
+# Setup Project
+    $ mkvirtualenv abc -p /usr/bin/python3.5
+    $ pip install -r requirements.txt
--- tracker/api/views.py
+++ tracker/api/views.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 

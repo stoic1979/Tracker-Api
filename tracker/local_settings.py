08de201fd6d6b8f4e55780f4fc9b567be96e850f
Navjot Singh <weavebytes@gmail.com>
2018-03-12 15:10:06 +0530

    local setts

---
+++ tracker/local_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hyqgmjcz',
+        'USER': 'hyqgmjcz',
+        'PASSWORD': 'b68BPNaDYrKnlSRcAs-zU-VOUL0XILC5',
+        'HOST': 'baasu.db.elephantsql.com',
+        'PORT': '',
+    }
+}
+
+print ("Local Settings loaded... ")
